da72b61b750cd39631137fec188e97e52d2edaea
Andrew Tieszen <pilottzn@gmail.com>
2022-03-05 12:50:09 -0700

    Added debug info toggle (TAB)

--- The Backrooms - 1991.cpp
+++ The Backrooms - 1991.cpp
@@ -6,26 +6,42 @@ using namespace std;
 
 #include <SFML/Graphics.hpp>
 
-// Startup Settings
+// Startup settings
 const string title = "The Backrooms: 1991";
+bool showDebugInfo = false, toggleDebugInfo = false;
 int frameRate = 60;
 
-// Global SFML Objects
+// Global SFML objects
 sf::RenderTexture buffer;
 
 
-int main()
-{
+int main() {
+    // Create window
     sf::RenderWindow window (sf::VideoMode(512, 448), title);
     window.setFramerateLimit (frameRate);
 
-    while (window.isOpen()) {
-        sf::Event event;
+    // Print startup info to terminal
+    cout << "Opening " << title << ". Press TAB to toggle debug information.\n";
 
+    while (window.isOpen()) {
+        // System window management
+        sf::Event event;
         while (window.pollEvent (event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
 
+        // Toggle debug output
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab)) {
+            if (!toggleDebugInfo) {
+                showDebugInfo = !showDebugInfo;
+                cout << "\n   > Show debug info : " << showDebugInfo;
+            }
+            toggleDebugInfo = true;
+        } else toggleDebugInfo = false;
+
+        // Game logc
+
+        // Update graphics
         window.clear (sf::Color::Black);
         window.display();
     }
