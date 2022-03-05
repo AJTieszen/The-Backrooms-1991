d1e6953969f4d2dafa38dada3343f909d686df88
Andrew Tieszen <pilottzn@gmail.com>
2022-03-05 12:29:44 -0700

    Opens graphical window using SFML

--- The Backrooms - 1991.cpp
+++ The Backrooms - 1991.cpp
@@ -4,7 +4,29 @@
 #include <iostream>
 using namespace std;
 
+#include <SFML/Graphics.hpp>
+
+// Startup Settings
+const string title = "The Backrooms: 1991";
+int frameRate = 60;
+
+// Global SFML Objects
+sf::RenderTexture buffer;
+
+
 int main()
 {
-    cout << "Compiles Successfully!\n";
+    sf::RenderWindow window (sf::VideoMode(512, 448), title);
+    window.setFramerateLimit (frameRate);
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent (event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        window.clear (sf::Color::Black);
+        window.display();
+    }
 }
